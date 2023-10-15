0a944d69b8ede6e1bf2f0345332b6255994f8b81
MANDAR KULKARNI <jkmandar@gmail.com>
2023-10-15 12:55:20 +0530

    Feature Ascii Art

--- app.sh
+++ app.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
+echo $'\n'
+toilet -f pagga.tlf "Linux Setup"
 
-echo "
- _      _                     _____      _               
-| |    (_)                   / ____|    | |              
-| |     _ _ __  _   ___  __ | (___   ___| |_ _   _ _ __  
-| |    | | '_ \| | | \ \/ /  \___ \ / _ \ __| | | | '_ \ 
-| |____| | | | | |_| |>  <   ____) |  __/ |_| |_| | |_) |
-|______|_|_| |_|\__,_/_/\_\ |_____/ \___|\__|\__,_| .__/ 
-                                                  | |    
-                                                  |_|    
+echo $'\n'"Script for personal linux setup"
 
-"
-echo "Script for personal linux setup"
-
-while read -p ""$'\n'""$'\n'""$'\n'"Choose the following options to execute the script "$'\n'" 1. Backup Script to setup backup "$'\n'" 2. Firewall Script to setup Firewall "$'\n'" 3. Application Script to setup application "$'\n'" Enter the script number to exeute script : " num; do
+while read -p $'\n'"Choose the following options to execute the script "$'\n'" 1. Backup Script to setup backup "$'\n'" 2. Firewall Script to setup Firewall "$'\n'" 3. Application Script to setup application "$'\n\n'" Enter the script number to exeute script : " num; do
     # body
     if [ "$num" == 1 ]; then
         echo "executing Backup Script"
@@ -28,15 +19,8 @@ while read -p ""$'\n'""$'\n'""$'\n'"Choose the following options to execute the 
         ./setup.sh
 
     else
-        #echo " :( Please Enter Valid Input (1 2 3)"
-        echo "
-
-                _______ _     _ _____ _______      ______  __   __ _______
-                |______  \___/    |      |         |_____]   \_/   |______
-                |______ _/   \_ __|__    |         |_____]    |    |______
-                                                                        
-
-        "
+        echo $'\n'" :( Please Enter Valid Input (1 2 3)"
+        toilet -f pagga.tlf "Exit Bye"
         break
     fi
 done
